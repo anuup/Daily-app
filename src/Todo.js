89193f692d3eb40b80996fa89ca0e3d1c25ec476
anuup <anupb44@gmail.com>
2022-04-16 10:25:13 +0530

    todo continues

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 function Todo() {
+    const[todos,setTodos]=useState([]);
+    const[todo,setTodo]=useState([]);
   return (
-    <div>
-
-<h1>Daily</h1>
-
+    <div className='todo-input'>
+        <h1>Daily</h1>
+        <input className='inp'  value={todo} type="text" onChange={(e)=>setTodo(e.target.value)} placeholder='Type your Todo' />
+        <button onClick={()=>{setTodos([...todo,todos])}}>Add</button>
+        {
+            todos.map((value)=>{
+                <p>{value}</p>
+            })
+        }
     </div>
+    
   )
 }
 
+// onClick={()=>{setTodos([...todos,todo])}}
 export default Todo
